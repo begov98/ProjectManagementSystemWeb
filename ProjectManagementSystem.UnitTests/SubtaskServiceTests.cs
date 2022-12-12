a0ce3d40c04b99b0625355566f85384998e6de75
begov98 <begovplamen@gmail.com>
2022-12-12 22:35:37 +0200

    Added test.

--- ProjectManagementSystem.UnitTests/SubtaskServiceTests.cs
+++ ProjectManagementSystem.UnitTests/SubtaskServiceTests.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Moq;
 using ProjectManagementSystem.Contracts;
 using ProjectManagementSystem.Controllers;
@@ -7,31 +11,51 @@ using ProjectManagementSystem.Data;
 using ProjectManagementSystem.Data.Models;
 using ProjectManagementSystem.Models;
 using ProjectManagementSystem.Services;
+using System.Data.Common;
 
 namespace ProjectManagementSystem.UnitTests
 {
     [TestFixture]
     public class SubtaskServiceTests
     {
-
-
-        [SetUp]
-        public void Setup()
-        {
-
-        }
+        public ISubtaskService _service;
 
         [Test]
-        public async Task TestSubtaskAdding()
+        public void TestSubtaskAdding()
         {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
 
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.EnsureCreated();
+            }
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Projects.Add(new Project { Id = 1, Name = "TestProject", Description = "TestDescr", Picture = "url", ProjectManagerId = context.Users.FirstOrDefault().Id });
+                context.SaveChanges();
+            }
+
+            var model = new AddSubtaskViewModel()
+            {
+                Name = "Test",
+                Description = "Test",
+                StatusId = 1,
+                ProjectId = 1,
+                CategoryId = 1
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                _service = new SubtaskService(context);
+                _service.AddSubtaskAsync(model);
+                context.SaveChanges();
+                var subtask = context.Subtasks.FirstOrDefault();
+                Assert.That(subtask, Is.Not.Null);
+            }
         }
 
-
-        [TearDown]
-        public void TearDown()
-        {
-            //applicationDbContext.Dispose();
-        }
     }
 }
