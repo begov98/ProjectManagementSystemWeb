aa36eb31e7ac60f46dc8b22ab989b15900e5e603
begov98 <begovplamen@gmail.com>
2022-12-12 22:44:58 +0200

    Deleted unecessary code.

--- ProjectManagementSystem.UnitTests/SubtaskServiceTests.cs
+++ ProjectManagementSystem.UnitTests/SubtaskServiceTests.cs
@@ -1,17 +1,3 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.VisualBasic.Syntax;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Moq;
-using ProjectManagementSystem.Contracts;
-using ProjectManagementSystem.Controllers;
-using ProjectManagementSystem.Data;
-using ProjectManagementSystem.Data.Models;
-using ProjectManagementSystem.Models;
-using ProjectManagementSystem.Services;
-using System.Data.Common;
 
 namespace ProjectManagementSystem.UnitTests
 {
